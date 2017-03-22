e302ebd50459a8b8541648e74f33b32c9b5929b9
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-21 18:08:51 -0600

    Begin setting up server file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Happy Hours'
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
