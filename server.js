b32db5a0eca77b6bdd000e1b679129a8d0621be8
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-23 15:01:39 -0600

    All server functions work

--- server.js
+++ server.js
@@ -66,6 +66,10 @@ app.get('/api/v1/drinkers/:id', (request, response) => {
     })
 })
 
+app.get('/api/v1/restaurants/:id/favorites', (request, response) => {
+
+})
+
 app.post('/api/v1/restaurants', (request, response) => {
   const { name, address, phone } = request.body;
   const restaurant = { name, address, phone, created_at: new Date };
@@ -118,27 +122,101 @@ app.post('/api/v1/drinkers', (request, response) => {
 })
 
 app.patch('/api/v1/restaurants', (request, response) => {
-
+  const { id, name, address, phone } = request.body;
+  database('restaurants').where('id', id).update({ name })
+    .then(() => {
+      database('restaurants').select()
+        .then((restaurants) => {
+          response.status(200).json(restaurants);
+        })
+        .catch((error) => {
+          console.error('Cannot update restaurant', error);
+          response.send(422, 'Cannot update restaurant');
+        })
+    })
 })
 
 app.patch('/api/v1/happyhours', (request, response) => {
-
+  const { id, drinks } = request.body;
+  database('happyhours').where('id', id).update({ drinks })
+    .then(() => {
+      database('happyhours').select()
+        .then((happyhours) => {
+          response.status(200).json(happyhours);
+        })
+        .catch((error) => {
+          console.error('Cannot update happy hour', error);
+          response.send(422, 'Cannot update happy hour');
+        })
+    })
 })
 
 app.patch('/api/v1/drinkers', (request, response) => {
-
+  const { id, fav_hh } = request.body;
+  database('drinkers').where('id', id).update({ fav_hh })
+    .then(() => {
+      database('drinkers').select()
+        .then((drinkers) => {
+          response.status(200).json(drinkers);
+        })
+        .catch((error) => {
+          console.error('Cannot update user', error);
+          response.send(422, 'Cannot update user info');
+        })
+    })
 })
 
-app.delete('/api/v1/restaurants', (request, response) => {
-
+app.delete('/api/v1/restaurants/:id', (request, response) => {
+  const { id } = request.params;
+  database('drinkers').where('fav_hh', id).update({fav_hh: null})
+  .then(() => {
+    database('happyhours').where('restaurant_id', id).del()
+  }).then(() => {
+    database('restaurants').where('id', id).del()
+    .then(() => {
+      database('restaurants').select()
+      .then((restaurants) => {
+        response.status(200).json(restaurants);
+      })
+      .catch((error) => {
+        console.error('Cannot delete restaurant', error);
+        response.send(422, 'Cannot delete restaurant');
+      })
+    })
+  })
 })
 
-app.delete('/api/v1/happyhours', (request, response) => {
-
+app.delete('/api/v1/happyhours/:id', (request, response) => {
+  const { id } = request.params;
+  database('happyhours').where('drinker_id', id).update({drinker_id: null})
+  .then(() => {
+    database('happyhours').where('id', id).del()
+    .then(() => {
+      database('happyhours').select()
+      .then((happyhours) => {
+        response.status(200).json(happyhours)
+      })
+      .catch((error) => {
+        console.error('Cannot delete happy hour', error);
+        response.send(422, 'Cannot delete happy hour');
+      })
+    })
+  })
 })
 
-app.delete('/api/v1/drinkers', (request, response) => {
-
+app.delete('/api/v1/drinkers/:id', (request, response) => {
+  const { id } = request.params;
+  database('drinkers').where('id', id).del()
+    .then(() => {
+      database('drinkers').select()
+        .then((drinkers) => {
+          response.status(200).json(drinkers);
+        })
+        .catch((error) => {
+          console.error('Cannot delete user', error);
+          response.send(422, 'Cannot delete user');
+        })
+    })
 })
 
 
