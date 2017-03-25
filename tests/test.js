d5dceb0d71d16d8d8ed4920b8c099c375b328364
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-03-25 11:04:05 -0600

    More tests

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test'
+
 const chai = require('chai');
 const expect = chai.expect;
 const app = require('../server.js')
@@ -9,8 +11,6 @@ chai.use(chaiHttp);
 
 const setUp = () => {
   beforeEach(function(done) {
-    database.migrate.rollback()
-    .then(function() {
       database.migrate.latest()
       .then(function() {
         return database.seed.run()
@@ -18,7 +18,6 @@ const setUp = () => {
           done();
         });
       });
-    });
   });
 
   afterEach((done)=>{
@@ -35,9 +34,9 @@ describe('Server', () => {
   });
 });
 
-describe('GET /api/v1/restaurants', () => {
+describe('/api/v1/restaurants', () => {
   setUp();
-  it('should return a JSON object of restaurants', (done) => {
+  it('GET should return a JSON object of restaurants', (done) => {
     chai.request(app)
     .get('/api/v1/restaurants')
     .end((err, res) => {
@@ -45,44 +44,82 @@ describe('GET /api/v1/restaurants', () => {
       expect(res).to.have.status(200)
       expect(res).to.be.json
       expect(res.body).to.be.a('array')
-      expect(res.body).to.have.length(29 )
+      expect(res.body).to.have.length(30)
       done()
     });
   });
+
+  it.skip('POST should')
 });
 
-describe('GET /api/v1/happyhours', () => {
+describe('/api/v1/happyhours', () => {
   setUp();
-  it('should return a JSON object of restaurants', (done) => {
+  it('should return a JSON object of happyhours', (done) => {
     chai.request(app)
-    .get('/api/v1/restaurants')
+    .get('/api/v1/happyhours')
     .end((err, res) => {
       if(err) {done(err)}
       expect(res).to.have.status(200)
       expect(res).to.be.json
       expect(res.body).to.be.a('array')
-      expect(res.body).to.have.length(29 )
+      expect(res.body).to.have.length(30)
       done()
     });
   });
+
+  it.skip('POST should')
 })
 
-describe('GET /api/v1/drinkers', () => {
+describe('/api/v1/drinkers', () => {
   setUp();
-  it('should return a JSON object of restaurants', (done) => {
+  it('GET should return a JSON object of users', (done) => {
     chai.request(app)
-    .get('/api/v1/restaurants')
+    .get('/api/v1/drinkers')
     .end((err, res) => {
       if(err) {done(err)}
       expect(res).to.have.status(200)
       expect(res).to.be.json
       expect(res.body).to.be.a('array')
-      expect(res.body).to.have.length(29 )
+      expect(res.body).to.have.length(30)
       done()
     });
   });
+
+  it.skip('POST should')
 });
 
 describe('GET /api/v1/restaurants/:id', (done) => {
-  
+  setUp();
+  it('should get a specific restaurant', (done) => {
+    chai.request(app)
+    .get('/api/v1/restaurants/1')
+    .end((err, res) => {
+      if(err) {done(err)}
+      expect(res).to.have.status(200)
+      expect(res).to.be.json
+      expect(res.body).to.be.a('array')
+      expect(res.body[0]).to.have.property('name')
+      expect(res.body[0]).to.have.property('address')
+      expect(res.body[0]).to.have.property('phone')
+      done()
+    })
+  })
+})
+
+describe('GET /api/v1/happyhours/:id', (done) => {
+  setUp();
+  it('should get a specific restaurant', (done) => {
+    chai.request(app)
+    .get('/api/v1/restaurants/1')
+    .end((err, res) => {
+      if(err) {done(err)}
+      expect(res).to.have.status(200)
+      expect(res).to.be.json
+      // expect(res.body).to.be.a('array')
+      // expect(res.body[0]).to.have.property('restaurant_id')
+      // expect(res.body[0]).to.have.property('address')
+      // expect(res.body[0]).to.have.property('phone')
+      done()
+    })
+  })
 })
